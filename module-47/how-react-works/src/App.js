71b3e49e686ffdbdcaea46a25f925444888a2bd7
Tareq35 <a.t.tariqulislam@gmail.com>
2023-03-03 12:46:02 +0600

    updated

---
+++ module-47/how-react-works/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Device from './components/Device/Device';
+import Watch from './components/Watch/Watch';
+
+function App() {
+  return (
+    <div className="App">
+      <Device name="uphone" price="50000"></Device>
+      <Watch></Watch>
+    </div>
+  );
+}
+
+export default App;
