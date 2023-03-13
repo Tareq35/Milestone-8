2944b91ae2d20aec4af46df00b97e308554ed4d1
Tareq35 <a.t.tariqulislam@gmail.com>
2023-03-13 11:04:42 +0600

    updated

--- module-48/ema-john-simple/src/componets/Cart/Cart.js
+++ module-48/ema-john-simple/src/componets/Cart/Cart.js
@@ -2,21 +2,23 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({ cart }) => {
-    console.log(cart);
+    // console.log(cart);
 
     let total = 0;
     let shipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-        total = total + product.price;
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
         shipping = shipping + product.shipping;
     }
     const tax = parseFloat((total * 0.1).toFixed(2));
-    const grandTotal = total + shipping + tax;
+    const grandTotal = (total + shipping + tax).toFixed(2);
 
     return (
         <div className='cart'>
             <h4>Order Summery</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Shipping Cost: ${shipping}</p>
             <p>Tax: ${tax}</p>
